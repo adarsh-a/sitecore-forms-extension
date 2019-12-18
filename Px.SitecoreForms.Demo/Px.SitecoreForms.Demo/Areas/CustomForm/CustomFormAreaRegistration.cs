53d85572418bc53265be3ff629dde8dde418f548
Adarsh <adarsh.bhautoo@gmail.com>
2019-12-18 15:20:55 +0400

    [Area Registration] - Add Route

---
+++ Px.SitecoreForms.Demo/Px.SitecoreForms.Demo/Areas/CustomForm/CustomFormAreaRegistration.cs
@@ -0,0 +1,31 @@
+﻿using System.Web.Mvc;
+
+namespace Px.SitecoreForms.Demo.Areas.CustomForm
+{
+    public class CustomFormAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "CustomForm";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+             "CustomForm",
+             "customform",
+             new { controller = "CustomForm", action = "Index", id = UrlParameter.Optional },
+             new[] { "Px.SitecoreForms.Demo.Controller" }
+         );
+
+            context.MapRoute(
+                "CustomForm_default",
+                "CustomForm/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
